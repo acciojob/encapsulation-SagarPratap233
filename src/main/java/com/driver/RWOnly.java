251f72603dde298feaceefd813f307cebdfc6709
SagarPratap233 <sagarpratapsp@gmail.com>
2023-09-03 17:02:36 +0530

    First Attempt

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String variable;
+
+
+    public String getVariable() {
+        return variable;
+    }
+
+    public void setVariable(String variable) {
+        this.variable = variable;
+    }
 }
